a666ab75a40eea8914863d5ff4b3958ccdda3d28
Ashlesha Katepallewar <kahlesha2002@gmail.com>
2024-01-03 14:03:56 +0530

    firebase

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -18,20 +18,39 @@
 
 
 
-import firebase from 'firebase/compat/app'
- import 'firebase/compat/auth';
- import 'firebase/compat/database'
+// import firebase from 'firebase/compat/app'
+//  import 'firebase/compat/auth';
+//  import 'firebase/compat/database'
  
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBBOv9KNIVTsNwr_tSGAplO659p76iqIOA",
+//   authDomain: "food-waste-a0b20.firebaseapp.com",
+//   projectId: "food-waste-a0b20",
+//   storageBucket: "food-waste-a0b20.appspot.com",
+//   messagingSenderId: "349467816262",
+//   appId: "1:349467816262:web:b3205a8e603352cd5fb547",
+//   measurementId: "G-T52BKRR0RQ"
+// };
+
+// // Initialize Firebase
+// const app = firebase.initializeApp(firebaseConfig);
+// export default app 
+
+// Import the functions you need from the SDKs you need
+  import firebase from 'firebase/compat/app'
+  import 'firebase/compat/auth';
+  import 'firebase/compat/database'
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBBOv9KNIVTsNwr_tSGAplO659p76iqIOA",
-  authDomain: "food-waste-a0b20.firebaseapp.com",
-  projectId: "food-waste-a0b20",
-  storageBucket: "food-waste-a0b20.appspot.com",
-  messagingSenderId: "349467816262",
-  appId: "1:349467816262:web:b3205a8e603352cd5fb547",
-  measurementId: "G-T52BKRR0RQ"
+  apiKey: "AIzaSyD__G90ZEG0DjM1GtRiNIlu6I73dfvMnWY",
+  authDomain: "food-harvest-3c4ad.firebaseapp.com",
+  projectId: "food-harvest-3c4ad",
+  storageBucket: "food-harvest-3c4ad.appspot.com",
+  messagingSenderId: "341928350239",
+  appId: "1:341928350239:web:d29ccd7d669a097894fb2a",
+  measurementId: "G-0MMERRR4E8"
 };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
-export default app 
+ export default app
